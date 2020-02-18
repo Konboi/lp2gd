aea258a803959d740448372eca2ed53c4d9f8de8
Konboi <ryosuke.yabuki@gmail.com>
2020-02-18 12:25:11 +0900

    fix webhool handler

--- webhook.go
+++ webhook.go
@@ -1,64 +1,81 @@
 package lp2gp
 
 import (
-	"bytes"
-	"io/ioutil"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"cloud.google.com/go/pubsub"
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 const ContentEndpoint = "https://api-data.line.me/v2/bot/message/%s/content"
 
-type Request struct {
-	Events []*linebot.Event `json:"events"`
-}
-
 func WebHookHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
-	secret := os.Getenv("CHANNEL_SECRET")
+	secret := os.Getenv("LINE_CHANNEL_SECRET")
 	if secret == "" {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
+	projectID := os.Getenv("GCP_PUBSUB_PROJECT_ID")
+	if projectID == "" {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	log.Printf("body:%s", string(body))
+	topic := os.Getenv("GCP_PUBSUB_TOPIC")
+	if topic == "" {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
-	r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 	events, err := linebot.ParseRequest(secret, r)
 	if err != nil {
-		log.Printf("[ERROR] web hook json encode faild. error:%v\nsecret:%s, header:%s", err, secret, r.Header.Get("X-Line-Signature"))
+		log.Printf("[ERROR] web hook json encode faild. error:%v", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
+	ids := []string{}
 	for _, e := range events {
 		if e.Type != linebot.EventTypeMessage {
 			continue
 		}
 
 		switch msg := e.Message.(type) {
-		case *linebot.TextMessage:
-			log.Printf("[INFO] text message %s", msg.Text)
 		case *linebot.ImageMessage:
-			log.Println("--- content ---")
-			log.Printf(ContentEndpoint, msg.ID)
-			log.Printf("[INFO] image message. id:%s original:%s preview:%s\nmsg:%+v", msg.ID, msg.OriginalContentURL, msg.PreviewImageURL, msg)
+			ids = append(ids, msg.ID)
 		default:
-			log.Printf("[ERROR] unsupport message type. type:%T", e.Message)
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			return
+			continue
 		}
-		log.Printf("[INFO] web hook message. eventt: %+v", e)
 	}
-	return
+
+	if len(ids) == 0 {
+		fmt.Fprintf(w, "OK")
+	}
+
+	msg := Message{
+		ContentIDs: ids,
+	}
+
+	client, err := pubsub.NewClient(r.Context(), projectID)
+	if err != nil {
+		log.Printf("[ERROR] error new pubsub client. err:%v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	data, err := json.Marshal(msg)
+	if err != nil {
+		log.Printf("[ERROR] error mershal message. err:%v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	t := client.Topic(topic)
+	t.Publish(r.Context(), &pubsub.Message{Data: data})
 }
